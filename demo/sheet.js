0c9f3c747c9cad0da303c610c326ae847a173890
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2019-05-06 05:01:14 -0400

    Add format as type

--- demo/sheet.js
+++ demo/sheet.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     const data = [
         ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
-        ['2017', 10, 11, 12, 13],
-        ['2018', 20, 11, 14, 13],
-        ['2019', 30, 15, 12, 13]
+        ['2017', 10, 11, '2017-01-01', 13],
+        ['2018', 20, 11, '2018-12-12', 13],
+        ['2019', 30, 15, '2019-30-30', 13]
     ];
 
     const container = document.getElementById('sheet');
@@ -12,6 +12,36 @@ window.onload = () => {
         rowHeaders: true,
         colHeaders: true,
         filters: true,
-        dropdownMenu: true
+        dropdownMenu: true,
+        licenseKey: 'non-commercial-and-evaluation'
     });
+    window.Handsontable = Handsontable;
+    window.hot = hot;
+};
+
+columns = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4
+};
+
+typeConfigs = {
+    'date': {
+        correctFormat: true,
+        dateFormat: 'MM/DD/YYYY'
+    }
+};
+
+window.hotFormat = (columnLabel, type) => {
+    for (let i = 1; i < 4; i++) {
+        window.hot.setCellMetaObject(i, columns[columnLabel], {
+            ...typeConfigs[type],
+            validator: type,
+            renderer: type,
+            editor: type
+        });
+    }
+    window.hot.validateCells();
 };
